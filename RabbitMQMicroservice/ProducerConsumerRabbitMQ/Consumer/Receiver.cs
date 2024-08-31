f9c94e6458c9d148dd5cd162daab349379404320
Alan Rodrigues Alves Pereira <alanrodriguesdev@outlook.com>
2024-08-31 17:30:48 -0300

    feat(RabbitMQ): building the Consumer

---
+++ RabbitMQMicroservice/ProducerConsumerRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost"};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    string strQueue = "BasicTest";
+
+    channel.QueueDeclare(strQueue,false,false,false,null);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+
+        Console.WriteLine($"Received message {message}");
+    };
+
+    channel.BasicConsume(strQueue,true,consumer);
+    Console.WriteLine("Press [Enter] to exitthe consumer");
+    Console.ReadLine();
+}
